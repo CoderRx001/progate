bb9adc6cf228943ea336ce24c756a815e3431af7
CoderRx001 <carrillo964@gmail.com>
2018-04-19 22:25:06 -0500

    JS level 1

--- python/L1/calculations.py
+++
@@ -1,8 +0,0 @@
-# Print the result of 9 / 2
-print(9 / 2)
-
-# Print the result of 7 * 5
-print(7 * 5)
-
-# Print the remainder of 5 divided by 2 using %
-print(5 % 2)
--- python/L1/else_statements.py
+++
@@ -1,8 +0,0 @@
-money = 2
-apple_price = 4
-
-if money >= apple_price:
-    print('You can buy an apple')
-# When the condition above is False, print 'You do not have enough money'
-else: 
-    print('You do not have enough money')
