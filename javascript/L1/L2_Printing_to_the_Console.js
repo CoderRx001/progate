b01d224e1837bd66804ef093ec996a18ac8bad4d
CoderRx001 <carrillo964@gmail.com>
2018-04-20 02:51:20 -0500

    python level 1

--- javascript/L1/L2_Printing_to_the_Console.js
+++
@@ -1,8 +0,0 @@
-// Print "Hello World"
-console.log("Hello World");
-
-// Print "Studying Javascript!"
-console.log("Studying Javascript!");
-
-// Make the following line a comment
-//console.log("This should not be printed");
